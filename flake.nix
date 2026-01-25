e7194fd2069f284332bb3512884d34273e7e23ea
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2026-01-25 15:21:20 +0900

    chore: add flake.nix

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      systems,
+      ...
+    }:
+    let
+      eachSystem =
+        f:
+        nixpkgs.lib.genAttrs (import systems) (
+          system:
+          f {
+            inherit system;
+            pkgs = nixpkgs.legacyPackages.${system};
+          }
+        );
+    in
+    {
+      devShells = eachSystem (
+        { pkgs, ... }:
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              gitleaks
+              nixfmt
+              nodejs_24
+            ];
+          };
+        }
+      );
+    };
+}
