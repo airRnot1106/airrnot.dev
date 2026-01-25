ba611b6aff7bb297f3c8b8f3637af43550ed749b
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2026-01-25 15:42:46 +0900

    chore: init linter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,15 @@
+import { keepSortedCss } from '@airrnot/keep-sorted-css';
+// import panda from '@pandacss/eslint-plugin';
 import nextVitals from 'eslint-config-next/core-web-vitals';
 import nextTs from 'eslint-config-next/typescript';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import { builtinCommands } from 'eslint-plugin-command/commands';
+import command from 'eslint-plugin-command/config';
+import oxlint from 'eslint-plugin-oxlint';
+import react from 'eslint-plugin-react';
+import reactCompiler from 'eslint-plugin-react-compiler';
+import reactYouMightNotNeedAnEffect from 'eslint-plugin-react-you-might-not-need-an-effect';
+import storybook from 'eslint-plugin-storybook';
 import { defineConfig, globalIgnores } from 'eslint/config';
 
 const eslintConfig = defineConfig([
@@ -12,7 +22,40 @@ const eslintConfig = defineConfig([
     'out/**',
     'build/**',
     'next-env.d.ts',
+    'styled-system/**',
+    '!.storybook',
   ]),
+  {
+    plugins: {
+      react,
+    },
+    rules: {
+      'react/jsx-sort-props': [
+        'error',
+        {
+          callbacksLast: true,
+          ignoreCase: true,
+          reservedFirst: true,
+        },
+      ],
+    },
+  },
+  reactCompiler.configs.recommended,
+  reactYouMightNotNeedAnEffect.configs.recommended,
+  // {
+  //   name: 'panda/recommended',
+  //   plugins: { '@pandacss': panda },
+  //   rules: {
+  //     ...panda.configs.recommended.rules,
+  //     '@pandacss/no-hardcoded-color': 'off',
+  //   },
+  // },
+  ...storybook.configs['flat/recommended'],
+  command({
+    commands: [...builtinCommands, keepSortedCss],
+  }),
+  eslintConfigPrettier,
+  ...oxlint.buildFromOxlintConfigFile('.oxlintrc.json'),
 ]);
 
 export default eslintConfig;
